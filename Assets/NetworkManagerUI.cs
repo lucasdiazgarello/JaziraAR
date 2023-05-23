4f80e0d1944bd72a2d9a46f807a5b2fe1f46fd37
lucasdiazgarello <lucasdiazgarello@gmail.com>
2023-05-23 09:57:52 -0300

    Nerworking

---
+++ Assets/NetworkManagerUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+
+
+public class NetworkManagerUI : MonoBehaviour
+{
+
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button joinBtn;
+
+
+    private void Awake()
+    {
+
+        hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        joinBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+
+
+    }
+}
